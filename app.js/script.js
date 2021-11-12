1a5a26c367faed31e81e83c9619264801922df54
dee chao <chaoserady@gmail.com>
2021-11-12 12:59:14 -0600

    added media queries for lineup section

--- app.js/script.js
+++ app.js/script.js
@@ -43,44 +43,9 @@ const pirateName = document.querySelector(".pirate-name");
 const moonRockName = document.querySelector(".moonrock-name");
 const OxfordTanName = document.querySelector(".oxford-name");
 
-turtle.addEventListener("mouseover", () => {
-  turtleName.style.transform = "translateY(0)";
-  turtleName.style.opacity = "1";
-});
-
-turtle.addEventListener("mouseout", () => {
-  turtleName.style.transform = "translateY(200%)";
-  turtleName.style.opacity = "0";
-});
-
-pirate.addEventListener("mouseover", () => {
-  pirateName.style.transform = "translateY(0)";
-  pirateName.style.opacity = "1";
-});
-
-pirate.addEventListener("mouseout", () => {
-  pirateName.style.transform = "translateY(200%)";
-  pirateName.style.opacity = "0";
-});
-
-moonRock.addEventListener("mouseover", () => {
-  moonRockName.style.transform = "translateY(0)";
-  moonRockName.style.opacity = "1";
-});
-
-moonRock.addEventListener("mouseout", () => {
-  moonRockName.style.transform = "translateY(200%)";
-  moonRockName.style.opacity = "0";
-});
-
-oxfordTan.addEventListener("mouseover", () => {
-  OxfordTanName.style.transform = "translateY(0)";
-  OxfordTanName.style.opacity = "1";
-});
-
-oxfordTan.addEventListener("mouseout", () => {
-  OxfordTanName.style.transform = "translateY(200%)";
-  OxfordTanName.style.opacity = "0";
+const matchingMedia = matchMedia("(min-width: 1224px)");
+matchingMedia.addEventListener("change", () => {
+  console.log(matchingMedia);
 });
 
 // GALLERY SECTION //
